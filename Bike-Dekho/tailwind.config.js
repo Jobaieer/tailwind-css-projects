3fbd24c1dde3ffd0e6cb133cfbc62f02bf2175a1
Jobaieer <jobaieer.cse@proton.me>
2024-10-26 20:39:37 +0600

    [bike-dekho] config file - added font family and themes colors.

--- Bike-Dekho/tailwind.config.js
+++ Bike-Dekho/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ['light', 'dark', 'cupcake'],
+    theme: {
+      colors: {
+        primary: '#3490dc',
+        secondary: '#ffed4a',
+        accent: '#e3342f',
+        neutral: '#ffffff',
+        base: '#000000',
+      },
+    },
+  },
 };
